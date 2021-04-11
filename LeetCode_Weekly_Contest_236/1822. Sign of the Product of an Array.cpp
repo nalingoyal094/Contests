b12fee79dbc1f85aa8da9eaf6d7c3e06ae7ad5cb
nalingoyal094 <52338664+nalingoyal094@users.noreply.github.com>
2021-04-11 16:11:02 +0530

    Create 1822. Sign of the Product of an Array.cpp

---
+++ LeetCode_Weekly_Contest_236/1822. Sign of the Product of an Array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int cnt=0;
+        for (int i : nums)
+        {
+            if (i==0)
+            {
+                return 0;
+            }
+            if (i<0)
+            {
+                cnt++;
+            }
+        }
+        if (cnt%2==0)
+        {
+            return 1;
+        }
+        else
+        {
+            return -1;
+        }
+        
+    }
+};
