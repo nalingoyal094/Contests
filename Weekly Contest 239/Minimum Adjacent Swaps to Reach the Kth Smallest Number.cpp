1af17b6a49851352decafe68dbed1f52946cd97f
nalingoyal094 <52338664+nalingoyal094@users.noreply.github.com>
2021-05-02 20:15:16 +0530

    Create Minimum Adjacent Swaps to Reach the Kth Smallest Number.cpp

---
+++ Weekly Contest 239/Minimum Adjacent Swaps to Reach the Kth Smallest Number.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int getMinSwaps(string num, int k) {
+        
+        int i=0;
+        string str=num;
+        do
+        {
+            i++;
+            if (i==k+1)
+            {
+                cout<<str<<endl;
+                break;
+            }
+            
+        }
+        while(next_permutation(str.begin(),str.end()));
+        int ans=0;
+        i=0;
+        int j=0;
+        while(i<num.size())
+        {
+            
+            if (str[i]==num[j])
+            {
+                i++;j++;
+                continue;
+            }
+            
+            if (str[i]!=num[j])
+            {
+                while(num[j]!=str[i])
+                {
+                    j++;
+                }
+                
+                while(i!=j)
+                {
+                    swap(num[j],num[j-1]);
+                    ans++;
+                    j--;
+                }
+                
+            }
+            i++;j++;
+            
+        }
+        
+     return ans;   
+        
+    }
+};
